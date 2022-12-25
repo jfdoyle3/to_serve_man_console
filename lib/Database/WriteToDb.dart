83ab75ae6dd43a4207dc7b1d375cff9ac4cb07e8
jfdoyle3 <indellibleink@gmail.com>
2022-12-25 11:40:44 -0500

    refactored

--- lib/Database/WriteToDb.dart
+++ lib/Database/WriteToDb.dart
@@ -3,6 +3,7 @@ import 'dart:io' show Platform;
 
 import '../Models/ingredient.dart';
 import '../Models/recipe.dart';
+import '../Models/step.dart';
 
 String host = Platform.environment['MONGO_DART_DRIVER_HOST'] ?? '127.0.0.1';
 String port = Platform.environment['MONGO_DART_DRIVER_PORT'] ?? '27017';
@@ -10,7 +11,7 @@ String port = Platform.environment['MONGO_DART_DRIVER_PORT'] ?? '27017';
 
 // Create / Write one recipe to the database.
 void main() async {
-  var db = Db('mongodb://$host:$port/Cookbook');
+  Db db = Db('mongodb://$host:$port/Cookbook');
   // Example url for Atlas connection
   /* var db = Db('mongodb://<atlas-user>:<atlas-password>@'
       'cluster0-shard-00-02.xtest.mongodb.net:27017,'
@@ -24,16 +25,16 @@ void main() async {
     Ingredient('egg', 'large',1,'-'),
     Ingredient('flour', 'AP', 1,'cups'),
   ], [
-    '1. mix ingredients',
-    '2. put batter in pan'
+    Step('1. mix ingredients'),
+    Step('2. put batter in pan')
   ]);
   Recipe pie = Recipe('Apple Pie', [
     Ingredient('apple','Delicious' ,10,'-'),
     Ingredient('flour','AP', 5, 'cups'),
     Ingredient('egg','Large', 1,'-')
   ], [
-    'mix ingredients',
-    'put apples and batter in pie plate'
+    Step('mix ingredients'),
+    Step('put apples and batter in pie plate')
   ]);
   // Map recipeMap = <String, Map>{};
 
@@ -41,7 +42,7 @@ void main() async {
   await db.drop();
   print('====================================================================');
   print('>> Adding Recipe');
-  var collection = db.collection('recipe');
+  DbCollection collection = db.collection('recipe');
   await collection.insert(
     {
       'name': cake.recipeName,
